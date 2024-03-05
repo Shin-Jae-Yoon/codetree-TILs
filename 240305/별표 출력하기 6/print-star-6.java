ae2ff81ba28637a990f81702855a4300046fc724
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-03-05 18:39:10 +0900

    Title: 별표 출력하기 6, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/별표 출력하기 6/print-star-6.java
+++ 240305/별표 출력하기 6/print-star-6.java
@@ -10,19 +10,19 @@ public class Main {
                 System.out.print("  ");
             }
 
-            for (int j = 1; j <= n - (2 * i) + 5; j++) {
+            for (int j = 1; j <= (2 * n) - (2 * i) + 1; j++) {
                 System.out.print("* ");
             }
 
             System.out.println();
         }
 
-        for (int i = 1; i <= n - 1; i++) {
-            for (int j = 1; j <= n - i - 1; j++) {
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i - 1; j++) {
                 System.out.print("  ");
             }
 
-            for (int j = 1; j <= n + (2 * i) - 3; j++) {
+            for (int j = 1; j <= (2 * n) - (2 * i) + 1; j++) {
                 System.out.print("* ");
             }
 
