08d2448469fbb443bd48c5a8a3bbf5262c5c9142
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-03-21 20:13:59 +0900

    Title: 연속부분수열일까, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/연속부분수열일까/contiguous-array-or-not.java
+++ 240321/연속부분수열일까/contiguous-array-or-not.java
@@ -6,21 +6,21 @@ public class Main {
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
 
-        int[] n1Arr = new int[n1 + 1];
-        int[] n2Arr = new int[n2 + 1];
+        int[] n1Arr = new int[n1];
+        int[] n2Arr = new int[n2];
 
-        for (int i = 1; i <= n1; i++) {
+        for (int i = 0; i < n1; i++) {
             n1Arr[i] = sc.nextInt();
         }
 
-        for (int i = 1; i <= n2; i++) {
+        for (int i = 0; i < n2; i++) {
             n2Arr[i] = sc.nextInt();
         }
 
-        for (int i = 1; i <= n1; i++) {
+        for (int i = 0; i < n1; i++) {
             boolean success = true;
 
-            for (int j = 1; j <= n2; j++) {
+            for (int j = 0; j < n2; j++) {
                 if (i + j >= n1) {
                     success = false;
                     break;
