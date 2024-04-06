2be1d42883237f54f00881cd5ad86e892e79016e
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-04-07 01:27:22 +0900

    Title: 격자 모양 출력하기, Time: 198ms, Memory: 10MB, Status: Passed - CodeTree

--- 240407/격자 모양 출력하기/print-grid-shape.java
+++ 240407/격자 모양 출력하기/print-grid-shape.java
@@ -15,7 +15,7 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(result[i][j] + " ");
             }
 
