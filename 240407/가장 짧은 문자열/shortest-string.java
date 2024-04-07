2fade7131d3c82422dc509a08a19d7395713980e
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-04-07 19:20:31 +0900

    Title: 가장 짧은 문자열, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240407/가장 짧은 문자열/shortest-string.java
+++ 240407/가장 짧은 문자열/shortest-string.java
@@ -11,32 +11,24 @@ public class Main {
         int len2 = str2.length();
         int len3 = str3.length();
 
-        int min, max;
+        int max = len1;
 
-        if (len1 >= len2 && len1 >= len3) {
-            max = len1;
-
-            if (len2 >= len3) {
-                min = len3;
-            } else {
-                min = len2;
-            }
-        } else if (len2 >= len1 && len2 >= len3) {
+        if (max < len2) {
             max = len2;
+        }
 
-            if (len1 >= len3) {
-                min = len3;
-            } else {
-                min = len1;
-            }
-        } else {
+        if (max < len3) {
             max = len3;
+        }
 
-            if (len2 >= len1) {
-                min = len1;
-            } else {
-                min = len2;
-            }
+        int min = len1;
+
+        if (min > len2) {
+            min = len2;
+        }
+
+        if (min > len3) {
+            min = len3;
         }
 
         System.out.print(max - min);
