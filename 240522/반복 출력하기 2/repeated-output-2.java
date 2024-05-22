113ea2d6ba227e77a5c3843f45107760c5bd8d1f
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-05-22 23:35:53 +0900

    Title: 반복 출력하기 2, Time: 240ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240522/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printHelloWorld(n);
+    }
+
+    private static void printHelloWorld(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        printHelloWorld(n - 1);
+        
+        System.out.println("HelloWorld");
+    }
+}
