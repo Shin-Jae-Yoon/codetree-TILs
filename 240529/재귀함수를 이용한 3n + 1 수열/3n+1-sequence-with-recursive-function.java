c491724b1a05d366a44fdee7384237b289637ab2
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-05-29 19:28:33 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240529/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
+++ 240529/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    private static int cnt = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        recursion(n);
-        System.out.println(cnt);
+        System.out.println(countNumber(n));
     }
 
-    private static int recursion(int n) {
+    private static int countNumber(int n) {
         if (n == 1) {
             return 0;
         }
 
         if (n % 2 == 0) {
             cnt++;
-            return recursion(n / 2);
+            return recursion(n / 2) + 1;
         }
 
         if (n % 2 == 1) {
             cnt++;
-            return recursion(n * 3 + 1);
+            return recursion(n * 3 + 1) + 1;
         }
 
         return 0;
