eee231704468cacc60ca4411201d317daf2f5997
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-06-27 20:28:17 +0900

    Title: 007, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240627/007/007.java
+++ 240627/007/007.java
@@ -4,25 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String secretCode = sc.next();
-        String meetingPoint = sc.next();
+        char meetingPoint = sc.next().charAt(0);
         int time = sc.nextInt();
 
-        Secret secret = new Secret(secretCode, meetingPoint, time);
+        Spy spy = new Spy(secretCode, meetingPoint, time);
 
-        System.out.println("secret code : " + secret.secretCode);
-        System.out.println("meeting point : " + secret.meetingPoint);
-        System.out.println("time : " + secret.time);
+        System.out.println("secret code : " + spy.getSecretCode());
+        System.out.println("meeting point : " + spy.getMeetingPoint());
+        System.out.println("time : " + spy.getTime());
     }
 }
 
-class Secret {
-    String secretCode;
-    String meetingPoint;
-    int time;
+class Spy {
+    private String secretCode;
+    private char meetingPoint;
+    private int time;
 
-    public Secret(String secretCode, String meetingPoint, int time) {
+    public Spy(String secretCode, char meetingPoint, int time) {
         this.secretCode = secretCode;
         this.meetingPoint = meetingPoint;
         this.time = time;
+    }
+
+    public String getSecretCode() {
+        return this.secretCode;
+    }
+
+    public char getMeetingPoint() {
+        return this.meetingPoint;
+    }
+
+    public int getTime() {
+        return this.time;
     }
 }
