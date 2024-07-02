3696d5407cc50f2bc2e09888fcc5a7bf51e41f81
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-02 17:47:42 +0900

    Title: Next Level, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240702/Next Level/next-level.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        int level = sc.nextInt();
+
+        User user = new User();
+
+        System.out.println("user " + user.id + " lv " + user.level);
+
+        user.id = id;
+        user.level = level;
+
+        System.out.println("user " + user.id + " lv " + user.level);
+
+    }
+}
+
+class User {
+    String id = "codetree";
+    int level = 10;
+
+    public User() {};
+
+    public User(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+}
