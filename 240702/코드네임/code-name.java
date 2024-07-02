1d0d11a600d9708e7dfff06b2587815c1259f658
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-02 18:02:09 +0900

    Title: 코드네임, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240702/코드네임/code-name.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Spy[] spys = new Spy[5];
+        
+        for (int i = 0; i < 5; i++) {
+            String codename = sc.next();
+            int score = sc.nextInt();
+
+            spys[i] = new Spy(codename, score);
+        }
+
+        int minIndex = 0;
+
+        for (int i = 0; i < 4; i++) {
+            if (spys[minIndex].score >= spys[i + 1].score) {
+                minIndex = i;
+            }
+        }
+
+        System.out.println(spys[minIndex].codename + " " + spys[minIndex].score);
+    }
+}
+
+class Spy {
+    String codename;
+    int score;
+
+    public Spy(String codename, int score) {
+        this.codename = codename;
+        this.score = score;
+    }
+}
