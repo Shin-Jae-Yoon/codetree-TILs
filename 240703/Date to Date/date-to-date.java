def761a814411ce978070604e2f42bcf99c84c93
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-03 20:15:08 +0900

    Title: Date to Date, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240703/Date to Date/date-to-date.java
+++ 240703/Date to Date/date-to-date.java
@@ -2,30 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numberOfDays = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-        int firstDaySum = 0;
-        
-        for (int i = 1; i < m1; i++) {
-            firstDaySum += numberOfDays[i];
+        System.out.println(numOfDays(m2, d2) - numOfDays(m1, d1) + 1);
+    }
+
+    private static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+
+        for (int i = 1; i < m; i++) {
+            totalDays += days[i];
         }
 
-        firstDaySum += d1;
+        totalDays += d;
 
-        int secondDaySum = 0;
-
-        for (int i = 1; i < m2; i++) {
-            secondDaySum += numberOfDays[i];
-        }
-
-        secondDaySum += d2;
-
-        System.out.println(secondDaySum - firstDaySum + 1);
+        return totalDays;
     }
 }
