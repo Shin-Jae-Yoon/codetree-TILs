8cf51a20eec36a49e20aaba98a57cdc0539950a2
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-05 00:02:49 +0900

    Title: 흰검 칠하기, Time: 249ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240705/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_K = 100000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] checked = new int[2 * MAX_K + 1];
+        int[] countBlack = new int[2 * MAX_K + 1];
+        int[] countWhite = new int[2 * MAX_K + 1];
+        int current = MAX_K;
+
+        for (int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            char direction = sc.next().charAt(0);
+
+            if (direction == 'R') {
+                for (int j = current; j < current + x; j++) {
+                    checked[j] = 1;
+                    countBlack[j]++;
+                }
+                current += x - 1;
+            }   
+
+            if (direction == 'L') {
+                for (int j = current; j > current - x; j--) {
+                    checked[j] = 2;
+                    countWhite[j]++;
+                }
+                current -= x - 1;
+            }
+        }
+
+        int white = 0;
+        int black = 0;
+        int gray = 0;
+
+        for (int i = 0; i <= 2 * MAX_K; i++) {
+            if (countBlack[i] >= 2 && countWhite[i] >= 2) {
+                gray++;
+            }
+            else if (checked[i] == 1) {
+                black++;
+            }
+            else if (checked[i] == 2) {
+                white++;
+            }
+        }
+
+        System.out.print(white + " " + black + " " + gray);
+    }
+}
