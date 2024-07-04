66716f4808fcad066a06e6d30f50a4f01a6ea410
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-04 16:16:00 +0900

    Title: 십진수와 이진수 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240704/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240704/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -5,13 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String n = sc.next();
 
+        // 십진수로 변환
         int num = 0;
         for (int i = 0; i < n.length(); i++) {
             num = num * 2 + (n.charAt(i) - '0');
         }
 
+        // 십진수에 17을 곱함
         num *= 17;
 
+        // 이진수로 변환
         int[] digits = new int[20];
         int cnt = 0;
 
@@ -25,6 +28,7 @@ public class Main {
             num /= 2;
         }
 
+        // 배열 순서 뒤집어 이진수 형태로 출력
         for (int i = cnt - 1; i >= 0; i--) {
             System.out.print(digits[i]);
         }
