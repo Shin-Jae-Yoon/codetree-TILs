300d5c5942a1f71f96ee18f2027cdb0e5a59fc84
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-04 14:20:10 +0900

    Title: 그 요일은, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240704/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String targetDay = sc.next();
+
+        int diff = numOfDays(m2, d2) - numOfDays(m1, d1);
+        int count = diff / 7 + 1;
+
+        System.out.println(count);
+    }
+
+    private static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int totalDays = 0;
+
+        for (int i = 1; i < m; i++) {
+            totalDays += days[i];
+        }
+
+        totalDays += d;
+
+        return totalDays;
+    }
+}
