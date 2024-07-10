5f1b16b5bf794f0846a76c5cdb5b9d5ad6879f68
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-07-11 00:33:29 +0900

    Title: 선두를 지켜라 3, Time: 250ms, Memory: 29MB, Status: Passed - CodeTree

--- 240711/선두를 지켜라 3/keep-the-lead-3.java
+++ 240711/선두를 지켜라 3/keep-the-lead-3.java
@@ -41,21 +41,21 @@ public class Main {
 
         for (int time = 1; time < timeA; time++) {
             if (positionA[time] > positionB[time]) {
-                if (leader == 2 || leader == 3 || time == 1) {
+                if (leader != 1) {
                     changeCount++;
                 }
 
                 leader = 1;
             }
             else if (positionA[time] < positionB[time]) {
-                if (leader == 1 || leader == 3 || time == 1) {
+                if (leader != 2) {
                     changeCount++;
                 }
 
                 leader = 2;
             }
-            else if (positionA[time] == positionB[time]) {
-                if (leader == 1 || leader == 2 || time == 1) {
+            else {
+                if (leader != 3) {
                     changeCount++;
                 }
 
