348552e51a065f70c77bc0f298be9d4d9eedaa7f
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-08-07 01:22:12 +0900

    Title: 씨 오 더블유 2, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240807/씨 오 더블유 2/c-o-w-2.java
+++ 240807/씨 오 더블유 2/c-o-w-2.java
@@ -14,7 +14,7 @@ public class Main {
 
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                for (int k = i + 2; k < n; k++) {
+                for (int k = j + 1; k < n; k++) {
                     if (input[i] == 'C' && input[j] == 'O' && input[k] == 'W') {
                         count++;
                     }
