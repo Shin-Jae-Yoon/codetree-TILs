e52f9c79525706007fee188766b25c780e45dd24
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-08-07 01:20:27 +0900

    Title: 씨 오 더블유 2, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240807/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[] input = br.readLine().toCharArray();
+
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = i + 2; k < n; k++) {
+                    if (input[i] == 'C' && input[j] == 'O' && input[k] == 'W') {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count);
+        System.out.print(sb.toString());
+    }
+}
