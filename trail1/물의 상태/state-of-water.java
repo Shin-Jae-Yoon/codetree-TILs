4d4f1b59b903c15b710d2f69be7dadafd7dac8b7
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-08-29 18:05:23 +0900

    [Passed] 물의 상태 (143ms, 10MB)

---
+++ trail1/물의 상태/state-of-water.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("ice");
+        }
+
+        if (n >= 100) {
+            System.out.println("vapor");
+        }
+
+        if (n >= 0 && n < 100) {
+            System.out.println("water");
+        }
+    }
+}
