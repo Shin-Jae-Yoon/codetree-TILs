91e77cd58e96b58bc48fe0290b45d0bf84a35e03
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-08-29 22:39:03 +0900

    [Passed] 알파벳에 따른 평가 (127ms, 10MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+
+        if (input == 'S') {
+            System.out.println("Superior");
+        } else if (input == 'A') {
+            System.out.println("Excellent");
+        } else if (input == 'B') {
+            System.out.println("Good");
+        } else if (input == 'C') {
+            System.out.println("Usually");
+        } else if (input == 'D') {
+            System.out.println("Effort");
+        } else {
+            System.out.println("Failure");
+        }
+    }
+}
