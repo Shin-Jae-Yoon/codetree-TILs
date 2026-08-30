8cfdf5e94743bf5cc4b52b016211c51a3a82669c
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-08-30 14:37:45 +0900

    [Passed] 등장횟수 확인 (125ms, 10MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int evenCount = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            int n = sc.nextInt();
+
+            if (n % 2 == 0) {
+                evenCount++;
+            }
+        }
+
+        System.out.print(evenCount);
+    }
+}
