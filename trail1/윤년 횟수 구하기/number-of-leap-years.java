92fea959604f2246d7171714d08136875e0d6bc1
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-08-30 14:35:47 +0900

    [Passed] 윤년 횟수 구하기 (140ms, 10MB)

---
+++ trail1/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int leapYearCount = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 4 == 0) {
+                if (i % 100 == 0 && i % 400 != 0) {
+                    continue;
+                }
+
+                leapYearCount++;
+            }
+        }
+
+        System.out.print(leapYearCount);
+    }
+}
