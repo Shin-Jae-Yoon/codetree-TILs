dc2e3e9ce820ce5a00b2e73b35299adf22487847
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-09-02 01:09:15 +0900

    [Passed] 별 그리기 (127ms, 10MB)

---
+++ trail1/별 그리기/star-drawing.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = i; j <= 3 * i - 2; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = i; j <= 3 * i - 2; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
