9400a23a8fffce8181480928b57fb2e812499dcb
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-09-02 01:44:00 +0900

    [Passed] 별표 출력하기 12 (131ms, 10MB)

--- trail1/별표 출력하기 12/print-star-12.java
+++ trail1/별표 출력하기 12/print-star-12.java
@@ -7,13 +7,20 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == 1 || (j % 2 == 0 && i <= j)) {
-                    System.out.print("* ");
+                if (j % 2 == 1) {
+                    if (i == 1) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
                 } else {
-                    System.out.print("  ");
+                    if (i <= j) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
                 }
             }
-
             System.out.println();
         }
     }
