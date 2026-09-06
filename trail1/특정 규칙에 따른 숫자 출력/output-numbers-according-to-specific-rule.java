9cfb4d9ff2d2633e4ae03de763284f6b62b84f01
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-09-06 22:42:14 +0900

    [Passed] 특정 규칙에 따른 숫자 출력 (133ms, 10MB)

---
+++ trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(count + " ");
+                    count++;
+                }
+
+                if (count == 10) {
+                    count = 1;
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
