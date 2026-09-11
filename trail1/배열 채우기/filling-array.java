fd108e3c3ee35cd88d51a1dd783ecb9890b3425d
Dev Uni <wlwhsvkdlxh@gmail.com>
2026-09-11 22:47:23 +0900

    [Passed] 배열 채우기 (130ms, 10MB)

--- trail1/배열 채우기/filling-array.java
+++ trail1/배열 채우기/filling-array.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[11];
-        int position = 10;
+        int position = 11;
 
         for (int i = 1; i <= 10; i++) {
             arr[i] = sc.nextInt();
             
             if (arr[i] == 0) {
-                position = i - 1;
+                position = i;
                 break;
             }
         }
 
-        for (int i = position; i >= 1; i--) {
+        for (int i = position - 1; i >= 1; i--) {
             System.out.print(arr[i] + " ");
         }
     }
